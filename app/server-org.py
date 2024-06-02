aa56a4229bfebd117f3a437b8e060ddb73640177
moosedev <ahmed.musawir@gmail.com>
2024-06-03 07:49:38 +0800

    3June2024 - simple updates

--- app/server-org.py
+++ app/server-org.py
@@ -1,17 +1,18 @@
+# app/server.py
+from decouple import config
 from fastapi import FastAPI
-from fastapi.responses import RedirectResponse
+from langchain_openai import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
 from langserve import add_routes
+
 
 app = FastAPI()
 
+model = ChatOpenAI(openai_api_key=config("OPENAI_API_KEY"))
+prompt = ChatPromptTemplate.from_template("Give me a summary about {topic} in a paragraph or less.")
+chain = prompt | model
 
-@app.get("/")
-async def redirect_root_to_docs():
-    return RedirectResponse("/docs")
-
-
-# Edit this to add the chain you want to add
-add_routes(app, NotImplemented)
+add_routes(app, chain, path="/openai")
 
 if __name__ == "__main__":
     import uvicorn
--- app/server.py
+++ app/server.py
@@ -2,17 +2,52 @@
 from decouple import config
 from fastapi import FastAPI
 from langchain_openai import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
+from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
+from fastapi.middleware.cors import CORSMiddleware
 from langserve import add_routes
 
 
 app = FastAPI()
 
+# -------------- CORS MIDDLWARE START ----------------
+# CORS middleware configuration
+origins = [
+    "http://localhost:4001",  # Add your frontend URL here
+    "http://127.0.0.1:4001",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# ------------- DEFAULT ROUTE START ----------------                
+
 model = ChatOpenAI(openai_api_key=config("OPENAI_API_KEY"))
 prompt = ChatPromptTemplate.from_template("Give me a summary about {topic} in a paragraph or less.")
+
 chain = prompt | model
 
 add_routes(app, chain, path="/openai")
+
+# ------------- DEFAULT ROUTE ENDS ----------------                
+
+# ------------- MOOSE ROUTE STARTS ----------------    
+moose_model = ChatOpenAI(openai_api_key=config("OPENAI_API_KEY"), streaming=True, temperature=0.7)
+moose_prompt = ChatPromptTemplate.from_template("{input}")
+
+add_routes(
+    app,
+    moose_prompt | moose_model,
+    path="/chat",
+    playground_type="default",
+    enable_feedback_endpoint=True,
+    enable_public_trace_link_endpoint=True,
+)
+# ------------- MOOSE ROUTE ENDS ----------------                
 
 if __name__ == "__main__":
     import uvicorn
